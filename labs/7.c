809fa240a5da9b5e988092fe2ffbcd86fc2de735
SelifanovKirill <44021038+SelifanovKirill@users.noreply.github.com>
2019-01-11 15:43:02 +0300

    Update 7.c

--- labs/7.c
+++ labs/7.c
@@ -1,141 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include <stdlib.h>
-
-struct array_of_structures {
-	float array[25];
-} base[5];
-// base[0] - a, base[1] - x, base[2] - G, base[3] - F, base[4] - Y.
 
 int main() {
-	
-FILE *file_write;
-file_write = fopen("functions.txt", "w");
-if (file_write == NULL) {
-	printf("File error");
-	return 1;
-}
-int iterations, i = 0, k = 0, j = 0, ran_a1, ran_a2, ran_x1, ran_x2, answer, amount_of_shablons = 0;
-float a, x, G, Y, F, min_a, min_x, max_a, max_x, shablon ;
-char stroka[30];
+    int index = 0;
+    double sx, ex, x, a, step;
+    char off, strfile[100];
+    FILE *file1, *file2;
 
-printf("Give a shablon to find it in results ");
-scanf("%f", &shablon);
-printf("How much operations this program should to do?\n");
-scanf("%d", &iterations);
-	
-for (i; i < iterations; i++) {
-		
-	printf("\nGive a range to 'a', that it can't across:\n");
-	scanf("%d %d", &ran_a1, &ran_a2);
-	printf("\nEnter 'a'\n");
-	scanf("%f", &a); //ask about &
-	base[0].array[i] = a;
-	if (a < ran_a1 || a > ran_a2) {
-		printf("\nYou entered the range of 'a' between %d and %d. \n'a' can't be used in this range!\n", ran_a1, ran_a2);
-		continue;
-	}
-	printf("\nGive a range to 'x', that it can't across:\n");
-	scanf("%d %d", &ran_x1, &ran_x2);
-	printf("\nEnter 'x'\n");
-	scanf("%f", &x);
-	base[1].array[i] = x;
-	if (x < ran_x1 || x > ran_x2) {
-		printf("\nYou entered the range of 'x' between %d and %d. \n'x' can't be used in this range!\n", ran_x1, ran_x2);
-		continue;
-	}
-	// G
-	if (a==0 && x==0) {
-		base[2].array[i] = 404.404;
-		fprintf(file_write,"%.3f\t", base[2].array[i]);
-		continue;
-	}
-	else {
-		G = (3*(4*pow(a,2) + 13*a*x + 9*pow(x,2)))/(10*pow(a,2) - 51*a*x + 5*pow(x,2));
-		if (G == shablon) {
-			amount_of_shablons++;
-		}
-		base[2].array[i] = G;
-		fprintf(file_write,"%.3f\t", base[2].array[i]);
-	}
-	// F
-	if ((-4<a<4) && (-4<x<4)) {
-		F = cosh(6*pow(a,2) + a*x - 2*pow(x,2));
-		if (F == shablon) {
-			amount_of_shablons++;
-		}
-		base[3].array[i] = F;
-		fprintf(file_write,"%.3f\t", base[3].array[i]); 
-	}
-	else {
-		base[3].array[i] = 404.404;
-		fprintf(file_write,"%.3f\t", base[3].array[i]);
-		continue;
-	}
-	// Y
-	if (a != 0 && x != 0) {
-		base[4].array[i] = 404.404;
-		fprintf(file_write,"%.3f\n", base[4].array[i]);
-		printf("\nThe results have written!");
-		continue;
-	}	
-	else {
-		Y = acos(14*pow(a,2) + 37*a*x + 5*pow(x,2) + 1);
-		if (Y == shablon) {
-			amount_of_shablons++;
-		}
-		base[4].array[i] = Y;
-		fprintf(file_write,"%.3f\n", base[4].array[i]);
-		printf("\nThe results have written!");
-	}
-	// breaking the calculations
-	printf("\nDo you want to stop the calculations? (write '1' to stop)\n");
-	scanf("%d",&answer);
-	if (answer == 1) {
-		printf("\n You answered '1'. Stopping the calculations...zzzz");
-		break;
-	}	
-	
-}
-// Calculating minimum and maximum arguments	
-min_a = base[0].array[0];
-max_a = base[0].array[0];
-min_x = base[1].array[0];
-max_x = base[1].array[0];
-for (k; k < iterations; k++) {
-	printf("\na[%d] is %f", k, base[0].array[k]);
-	printf("\nx[%d] is %f", k, base[1].array[k]);
-	if (base[0].array[k] > max_a){
-		max_a = base[0].array[k];
-	}
-	if (base[1].array[k] > max_x){
-		max_x = base[1].array[k];
-	}
-	if (base[0].array[k] < min_a) {
-		min_a = base[0].array[k];
-	}
-	if (base[1].array[k] < min_x) {
-		min_x = base[1].array[k];
-	}
-}
-printf("\nMin of 'a' is %f, max of 'a' is %f \nMin of 'x' is %f, max of 'x' is %f\n", min_a, max_a, min_x, max_x);
-fclose(file_write);
-// Clearing the array of structures
-memset(base, 0, sizeof(base));
-// Reading from file
-FILE *file_read;
-file_read = fopen("functions.txt", "r");
-printf("G\t  F\t\tY\n");
-for (j; fgets(stroka, 30, file_read) != NULL; j++) {
-	printf("%s", stroka);
-}
-fclose(file_read);
-if (amount_of_shablons > 0) {
-	printf("Where %d shablons\n========\n", amount_of_shablons);
-}
-else {
-	printf("No shablons\n========\n");
-}
-return 0;
+
+    for (;;) { /*start*/
+        printf("Введите начальное значение Х:");
+        scanf("%lf", &sx);
+        printf("Введите конечное значение Х:");
+        scanf("%lf", &ex);
+        printf("Введите значение A:");
+        scanf("%lf", &a);
+        printf("Введите шаг:");
+        scanf("%lf", &step);
+        struct {
+            double G [100];
+            double F [100];
+            double Y [100];
+        } result;
+
+        for (x = sx; x <= ex; x += step) {
+            if (fabs(35 * a * a + 37 * a * x + 6 * x * x) >= 0.0001) {
+                result.G[index] = (3 * (-3 * a * a + 2 * a * x + 21 * x * x) / (35 * a * a + 37 * a * x + 6 * x * x));
+            } else {
+                printf("При X = %lf, A = %lf, функцию G вычислить невозможно.\n", x, a);
+            }
+
+            if (fabs( sin(20 *(a * a)+ 9 * a * x - 20 *(x * x) - 3.14/2)) >= 0.0001) {
+                result.F[index] = (-1) / ( sin( 20 *(a * a) + 9 * a * x - 20 * (x * x) - 3.14/2));
+            } else {
+                printf("При X = %lf, A = %lf, функцию F вычислить невозможно.\n", x, a);
+            }
+
+            if ( fabs(a * a - 6 * a * x + 5 * (x * x) + 1) <= 1) {
+                result.Y[index] = acos(a * a - 6 * a * x + 5 * (x * x) + 1);
+            } else {
+                printf("При X = %lf, A = %lf, функцию Y вычислить невозможно.\n", x, a);
+            }
+
+            index += 1;
+        }
+        index = (int) sx;
+        if ((file1 = fopen("/home/user/Desktop/test.txt", "w")) == NULL){
+            printf("Ошибка(1)\n");
+        } else {
+            printf("Успешно(1)\n");
+        }
+        fprintf(file1, "Start:\n");
+        while (index <=ex) {
+            fprintf(file1, "G = %9.3lf, F = %9.3lf, Y = %9.3lf\n", result.G[index], result.F[index], result.Y[index]);
+            index += step;
+        }
+        fprintf(file1, "End.\n");
+        fclose(file1);
+
+        if ((file2 = fopen("/home/user/Desktop/test2.txt", "r")) == NULL){
+            printf("Ошибка(2)\n");
+        } else {
+            printf("Успешно(2)\n");
+        }
+        while (!feof(file2)){
+            fscanf(file2, "%s", strfile);
+            printf("%s", strfile);
+        }
+        fclose(file2);
+
+        memset(result.G, 0, 100);
+        memset(result.F, 0, 100);
+        memset(result.Y, 0, 100);
+        for(;;) { /*off*/
+            printf("\nПродолжить работу программы?(y/n)\n");
+            scanf("%s", &off);
+            switch (off) {
+                case 'y':
+                    break;
+                case 'n':
+                    exit(0);
+                default:
+                    printf("Неправильно введен ответ.\n");
+                    continue;
+            }
+            break;
+        }
+    }
 }
